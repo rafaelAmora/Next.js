38a149456ffdefc4c6d937dfcbca3f92068f135a
Rafael Amora <viniciusrafael183@gmail.com>
2024-02-27 22:20:10 -0300

    paginas estaticas no next

--- src/pages/dynamic.tsx
+++ src/pages/dynamic.tsx
@@ -1,20 +1,58 @@
 // pages/dynamic.tsx
 
-import { NextPage } from "next"
+import { GetServerSideProps, NextPage } from "next"
+import { ReactNode, useEffect, useState } from "react"
 import { Col, Container, Row } from "reactstrap"
+import { GetServerSidePropsContext} from "next"
 
-const Dynamic: NextPage = () => {
-  return (
+type ApiResponse = {
+  name: string
+  timestamp: Date
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+	const serverSideData: ApiResponse = await fetch(`${process.env.NEXT_PUBLIC_APIURL}/api/hello`).then(res => res.json())
+
+  return {
+    props: {
+      serverSideData
+    }
+  }
+}
+
+
+const Dynamic: NextPage  = (props : {
+  children?: ReactNode
+  serverSideData?: ApiResponse
+}) => {
+  const [clientSideData, setClientSideData] = useState<ApiResponse>()
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const fetchData = async () => {
+    const data = await fetch("/api/hello").then((res) => res.json())
+    setClientSideData(data)
+  }
+
+   return (
     <Container tag="main">
-      <h1 className="my-5">Como funcionam as renderizações do Next.js</h1>
+      <h1 className="my-5">
+        Como funcionam as renderizações do Next.js
+      </h1>
 
       <Row>
         <Col>
-          <h3>Gerado no servidor:</h3>
+          <h3>
+            Gerado no servidor: {props.serverSideData?.timestamp}
+          </h3>
         </Col>
 
         <Col>
-          <h3>Gerado no cliente:</h3>
+          <h3>
+            Gerado no cliente: {clientSideData?.timestamp}
+          </h3>
         </Col>
       </Row>
     </Container>
---
+++ src/pages/static.tsx
@@ -0,0 +1,41 @@
+// pages/static.tsx
+
+import { NextPage } from "next"
+import { useEffect, useState } from "react"
+import { Col, Container, Row } from "reactstrap"
+
+type ApiResponse = {
+  name: string
+  timestamp: Date
+}
+
+const Static: NextPage = () => {
+  const [clientSideData, setClientSideData] = useState<ApiResponse>()
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const fetchData = async () => {
+    const data = await fetch(`/api/hello`).then((res) => res.json())
+    setClientSideData(data)
+  }
+
+  return (
+    <Container tag="main">
+      <h1 className="my-5">Como funcionam as renderizações do Next.js</h1>
+
+      <Row>
+        <Col>
+          <h3>Gerado estaticamente durante o build:</h3>
+        </Col>
+
+        <Col>
+          <h3>Gerado no cliente: {clientSideData?.timestamp}</h3>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default Static
